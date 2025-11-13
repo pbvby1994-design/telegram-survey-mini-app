20eb5207f456d6ea505453bd85a2d8b74c9d06a1
Mikhail <pbvby1994@gmail.com>
2025-11-13 13:58:34 +0500

    new

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
-// !!! ВНИМАНИЕ: Все ключи и токены, расположенные здесь, будут доступны в браузере.
+// ВНИМАНИЕ: Все ключи и токены, расположенные здесь, будут доступны в браузере.
 
-// Конфигурация Firebase для клиента (публичный ключ)
+// Конфигурация Firebase для клиента (публичный ключ, скопирован из .env)
 export const FIREBASE_CONFIG = {
     apiKey: "AIzaSyDVeRXO16KtIwoJQb4uHQuCsEv3xTLBGhI",
     authDomain: "agitator-notebook.firebaseapp.com",
@@ -11,7 +11,7 @@ export const FIREBASE_CONFIG = {
     appId: "1:518555352011:web:2b18d460a6fa4acb66944e"
 };
 
-// Публичный токен Dadata (только для подсказок)
+// Публичный токен Dadata (скопирован из .env, используется для suggestions/address)
 export const DADATA_TOKEN = "29c85666d57139f459e452d1290dd73c23708472";
 
 // Параметры Dadata для фильтрации по регионам (ХМАО - KLADR 86)
