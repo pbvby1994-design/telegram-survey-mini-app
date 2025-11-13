584d6e72bf28158f18ba4fd4790b602170e5560f
Mikhail <pbvby1994@gmail.com>
2025-11-13 12:33:19 +0500

    new

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 // config.js
-//
 // !!! ВНИМАНИЕ: Все ключи и токены, расположенные здесь, будут доступны в браузере.
-// Секретный ключ Dadata (DADATA_SECRET) НЕ должен здесь находиться.
 
 // Конфигурация Firebase для клиента (публичный ключ)
-// Соответствует FIREBASE_CONFIG_JSON из вашего запроса
 export const FIREBASE_CONFIG = {
     apiKey: "AIzaSyDVeRXO16KtIwoJQb4uHQuCsEv3xTLBGhI",
     authDomain: "agitator-notebook.firebaseapp.com",
@@ -15,13 +12,17 @@ export const FIREBASE_CONFIG = {
 };
 
 // Публичный токен Dadata (только для подсказок)
-// (29c85666d57139f459e452d1290dd73c23708472)
 export const DADATA_TOKEN = "29c85666d57139f459e452d1290dd73c23708472";
 
 // Параметры Dadata для фильтрации по регионам (ХМАО - KLADR 86)
 export const DADATA_LOCATION_RESTRICTIONS = [
-    { 'kladr_id': '86' },
+    { 'kladr_id': '86' }, 
 ];
 
-// Для доступа к этим константам в других скриптах используйте:
-// import { FIREBASE_CONFIG, DADATA_TOKEN, DADATA_LOCATION_RESTRICTIONS } from './config.js';
+
+// --- КРИТИЧЕСКИЙ FIX ДЛЯ СОВМЕСТИМОСТИ ---
+// Назначение констант глобальному объекту window для доступа из немодульных скриптов
+window.FIREBASE_CONFIG = FIREBASE_CONFIG;
+window.DADATA_TOKEN = DADATA_TOKEN;
+window.DADATA_LOCATION_RESTRICTIONS = DADATA_LOCATION_RESTRICTIONS;
+// ------------------------------------------
